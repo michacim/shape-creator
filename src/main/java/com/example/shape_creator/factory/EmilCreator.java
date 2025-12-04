52ff54c2a15f5b67152fab51187b15c02d1ff00a
Emil Puls <emil.puls@92mx.de>
2025-12-04 14:18:10 +0100

    EmilCreator added

---
+++ src/main/java/com/example/shape_creator/factory/EmilCreator.java
@@ -0,0 +1,24 @@
+package com.example.shape_creator.factory;
+
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+
+public class EmilCreator extends ShapeCreator{
+    public EmilCreator(Pane canvas) {
+        super(canvas);
+    }
+
+    @Override
+    public Shape createShape() {
+        double radius = rnd.nextDouble(15,40);
+        Circle circle = new Circle(randomX(),randomY(),radius);
+        circle.setFill(Color.color(rnd.nextDouble(), rnd.nextDouble(), rnd.nextDouble(),0.6));
+        circle.setStroke(Color.BLUE);
+        circle.setEffect(new DropShadow(10, Color.BLACK));
+
+        return circle;
+    }
+}
